78bd9f31cff773f2eee26f3660234ae6406ed822
Delimond-Dev <delimonddev@localhost.localdomain>
2022-12-03 19:26:35 +0100

    app reusable config

--- src/app/index.js
+++ src/app/index.js
@@ -5,7 +5,24 @@ export default {
   install: (app, options) => {
     /* Global Components register */
     app.use(components);
-    app.use(globals);
     /* Global config */
+    app.use(globals);
+
+    /*Directives*/
+    app.directive("font-size", (el, binding, vnode) => {
+      var size = 16;
+      switch (binding.arg) {
+        case "small":
+          size = options.fontSize.small;
+          break;
+        case "large":
+          size = options.fontSize.large;
+          break;
+        default:
+          size = options.fontSize.medium;
+          break;
+      }
+      el.style.fontSize = size + "px";
+    });
   },
 };
--- src/router/routes/index.js
+++ src/router/routes/index.js
@@ -1,12 +1,12 @@
 const routes = [
   {
     path: "/",
-    component: () => import("@/view/pages/home.vue"),
+    component: () => import("@/views/pages/home.vue"),
     name: "home",
   },
   {
     path: "/features",
-    component: () => import("@/view/pages/features.vue"),
+    component: () => import("@/views/pages/features.vue"),
     name: "features",
   },
 ];
